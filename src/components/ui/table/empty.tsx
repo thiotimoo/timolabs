22875b6d4288b11c7ec2f8d73af4a07870dc036c
thiotimoo <timoothio@gmail.com>
2024-01-25 16:39:31 +0700

    Unicode Hotfix Lint

--- src/components/ui/table/empty.tsx
+++ src/components/ui/table/empty.tsx
@@ -4,7 +4,7 @@ export const EmptyTableBody = () => {
     return (
         <div className="h-32 relative">
             <div className="absolute w-full h-32 flex items-center justify-center">
-                <p className="text-center text-lg">There's nothing here.</p>
+                <p className="text-center text-lg">There is nothing here.</p>
             </div>
         </div>
     );
