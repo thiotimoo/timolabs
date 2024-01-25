869f49c02f4b021a26db381acebd4bd41e5ff40c
thiotimoo <timoothio@gmail.com>
2024-01-25 16:47:35 +0700

    authOptions Seperation

--- src/lib/auth-libs.ts
+++ src/lib/auth-libs.ts
@@ -1,6 +1,74 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
+import connectDatabase from "@/lib/connectDatabase";
+import User from "@/model/User";
+import githubAuth from "next-auth/providers/github";
+import { NextAuthOptions, getServerSession } from "next-auth";
+
 export const authUserSession = async() => {
     const session = await getServerSession(authOptions)
     return session?.user
 }
+
+export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        githubAuth({
+            clientId: process.env.GITHUB_CLIENT_ID ?? "",
+            clientSecret: process.env.GITHUB_CLIENT_SECRET ?? "",
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, user, account, profile, isNewUser }: any) {
+            await connectDatabase();
+            const username = token.name;
+            const email = token.email;
+            const role = "user";
+            const picture = token.picture;
+            const userCheck = await User.findOne({ email: email });
+            if (userCheck) {
+                return {
+                    ...token,
+                    createdAt: userCheck?.createdAt,
+                    updatedAt: userCheck?.updatedAt,
+                    picture: userCheck?.picture,
+                    email: userCheck?.email,
+                    username: userCheck?.username,
+                    role: userCheck?.role,
+                    _id: userCheck._id,
+                };
+            } else {
+                const newUserCheck = await User.create({
+                    username,
+                    email,
+                    role,
+                    picture,
+                });
+                return {
+                    ...token,
+                    createdAt: newUserCheck?.createdAt,
+                    updatedAt: newUserCheck?.updatedAt,
+                    picture: newUserCheck?.picture,
+                    email: newUserCheck?.email,
+                    username: newUserCheck?.username,
+                    role: userCheck?.role,
+                    _id: newUserCheck._id,
+                };
+            }
+        },
+        async session({ session, token, user }) {
+            session.user = token;
+            console.log(
+                "session_sessino",
+                session,
+                "session_token",
+                token,
+                "session_user",
+                user
+            );
+
+            return session;
+        },
+    },
+    session: {
+        strategy: "jwt",
+    },
+};
